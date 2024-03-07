8a5fcace323a72e42f4d8fd2d6e212ea780e8893
Bernardo Carvalho <nal-carvalho@hotmail.com>
2024-03-07 11:16:48 -0300

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,27 +8,6 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-
-# farm1 = Farm.create!(name: 'Sol Nascente', size: '500 ha', cep: '49075220', user_id: 1)
-# farm2 = Farm.create!(name: 'Sol Nascente', size: '500 ha', cep: '49075220', user_id: 1)
-
-# stock1 = Storage.create!(name: "Galpao principal", size: '20 m2', capacity: 1000, farm_id: 1)
-# stock2 = Storage.create!(name: "Galpao da laranja", size: '10 m2', capacity: 400, farm_id: 1)
-
-# cart1 = Cart.create!(date_move: '03/12/2022', approved: true, storage_id: 1)
-# cart2 = Cart.create!(date_move: '13/12/2022', approved: true, storage_id: 1)
-# cart3 = Cart.create!(date_move: '15/12/2022', approved: true, storage_id: 1)
-
-# chemicals1 = Chemical.create!(product_name: "Crucial", compound_product: "Glifosato", type_product: 'Herbicida', area: 'Milho,Laranja,Soja', measurement_unit: 'L', amount: 20)
-# chemicals2 = Chemical.create!(product_name: "N400", compound_product: "Nitrogenio", type_product: 'Adubo Foliar', area: 'Milho,Laranja,Soja', measurement_unit: 'L', amount: 20)
-# chemicals3 = Chemical.create!(product_name: "Atrazina Nortox 500 SC", compound_product: "Herbicida", type_product: 'Adubo Foliar', area: 'Milho,Laranja,Soja', measurement_unit: 'L', amount: 20)
-
-# c_chemical = CartChemical.create!(quantity: 10, chemical_id: 1, cart_id: 1)
-# c_chemica2 = CartChemical.create!(quantity: 10, chemical_id: 2, cart_id: 1)
-# c_chemica3 = CartChemical.create!(quantity: -5, chemical_id: 2, cart_id: 3)
-# c_chemica4 = CartChemical.create!(quantity: 10, chemical_id: 3, cart_id: 2)
-# c_chemica5 = CartChemical.create!(quantity: 10, chemical_id: 2, cart_id: 2)
-
 User.create!(
   email: "henrique@email.com",
   password: '123456',
@@ -38,3 +17,23 @@ User.create!(
   role: 0,
   cpf: "66699966669"
 )
+
+farm1 = Farm.create!(name: 'Sol Nascente', size: '500 ha', cep: '49075220', user_id: 1)
+farm2 = Farm.create!(name: 'Sol Nascente', size: '500 ha', cep: '49075220', user_id: 1)
+
+stock1 = Storage.create!(name: "Galpao principal", size: '20 m2', capacity: 1000, farm_id: 1)
+stock2 = Storage.create!(name: "Galpao da laranja", size: '10 m2', capacity: 400, farm_id: 1)
+
+cart1 = Cart.create!(date_move: '03/12/2022', approved: true, storage_id: 1)
+cart2 = Cart.create!(date_move: '13/12/2022', approved: true, storage_id: 1)
+cart3 = Cart.create!(date_move: '15/12/2022', approved: true, storage_id: 1)
+
+chemicals1 = Chemical.create!(product_name: "Crucial", compound_product: "Glifosato", type_product: 'Herbicida', area: 'Milho,Laranja,Soja', measurement_unit: 'L', amount: 20)
+chemicals2 = Chemical.create!(product_name: "N400", compound_product: "Nitrogenio", type_product: 'Adubo Foliar', area: 'Milho,Laranja,Soja', measurement_unit: 'L', amount: 20)
+chemicals3 = Chemical.create!(product_name: "Atrazina Nortox 500 SC", compound_product: "Herbicida", type_product: 'Adubo Foliar', area: 'Milho,Laranja,Soja', measurement_unit: 'L', amount: 20)
+
+c_chemical = CartChemical.create!(quantity: 10, chemical_id: 1, cart_id: 1)
+c_chemica2 = CartChemical.create!(quantity: 10, chemical_id: 2, cart_id: 1)
+c_chemica3 = CartChemical.create!(quantity: -5, chemical_id: 2, cart_id: 3)
+c_chemica4 = CartChemical.create!(quantity: 10, chemical_id: 3, cart_id: 2)
+c_chemica5 = CartChemical.create!(quantity: 10, chemical_id: 2, cart_id: 2)

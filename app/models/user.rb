ade3e8993e2371915dd62ad2d0e8f55e2459c6f3
Bernardo Carvalho <nal-carvalho@hotmail.com>
2024-03-07 14:51:12 -0300

    signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :farm
+  validates :first_name, :last_name, :address, :cpf, :role, presence: true
+
+  has_many :farms
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end

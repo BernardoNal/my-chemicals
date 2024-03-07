8975cb23c2420d77325b0b793b31c22cb257efd0
claraleal <clara.lidiax@gmail.com>
2024-03-07 16:15:49 -0300

    farms-show-controller

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -1,2 +1,27 @@
 class FarmsController < ApplicationController
+  def new
+    @farm = Farm.new
+  end
+
+  def create
+    @farm = Farm.new(farm_params)
+    @farm.user = current_user
+    @farm.save
+
+    if @farm.save
+      # redirect_to product_path(@product)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def myfarms
+    @farms = current_user.farms
+  end
+
+  private
+
+  def farm_params
+    params.require(:farm).permit(:name, :size, :complement, :cep)
+  end
 end
--- app/models/farm.rb
+++ app/models/farm.rb
@@ -2,4 +2,6 @@ class Farm < ApplicationRecord
   belongs_to :user
   has_many :storages, dependent: :destroy
   has_many :carts, through: :storages
+
+  validates :name, :size, :cep, presence: true
 end

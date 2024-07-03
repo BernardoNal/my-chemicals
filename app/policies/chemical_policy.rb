2d30efbb38aefa866f1ebb1bc59d355382535331
Bernardo Carvalho <nal-carvalho@hotmail.com>
2024-07-03 10:16:16 -0300

    feat: adjust in the user

--- app/policies/chemical_policy.rb
+++ app/policies/chemical_policy.rb
@@ -3,6 +3,18 @@ class ChemicalPolicy < ApplicationPolicy
     true
   end
 
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     # def resolve

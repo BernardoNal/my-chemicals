5271fb85ba39649cca260ebb34944413701ae001
Bernardo Carvalho <50677768+BernardoNal@users.noreply.github.com>
2024-07-03 16:14:52 -0300

    Close: Feature/#127 (#133)
    * feat: new and create chemical
    
    * feat: edit chemical
    
    * feat: index chemicals
    
    * feat: adding chemicals policy (admin)

--- app/controllers/chemicals_controller.rb
+++ app/controllers/chemicals_controller.rb
@@ -1,7 +1,67 @@
 class ChemicalsController < ApplicationController
+  before_action :set_chemical, only: %i[edit update destroy]
+
+  def index
+    @chemicals = policy_scope(Chemical).limit(20)
+    authorize @chemicals
+    if params[:product_name].present?
+      @chemicals = @chemicals.where('product_name ILIKE ?', "%#{params[:product_name]}%")
+    end
+  end
+
   # Displays details of a specific chemical
   def show
     @chemical = Chemical.find(params[:id])
     authorize @chemical
+  end
+
+  def new
+    @chemical = Chemical.new
+    authorize @chemical
+  end
+
+  # Creates a new chemical
+  def create
+    @chemical = Chemical.new(chemical_params)
+    authorize @chemical
+    if @chemical.save
+      flash[:alert] = "Químico criado com sucesso."
+
+      redirect_to root_path
+      # redirect_to mychemicals_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # Renders form to edit a chemical
+  def edit
+    authorize @chemical
+  end
+
+  # Updates a chemical
+  def update
+    authorize @chemical
+    if @chemical.update(chemical_params)
+      flash[:alert] = "Químico Alterado com sucesso."
+
+      redirect_to root_path
+      # redirect_to mychemicals_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+
+  # Permits chemical parameters
+  def chemical_params
+    params.require(:chemical).permit(:product_name, :compound_product, :type_product, :area, :measurement_unit, :amount)
+  end
+
+  # Sets the chemical instance variable based on the provided id
+  def set_chemical
+    @chemical = Chemical.find(params[:id])
   end
 end
--- app/models/chemical.rb
+++ app/models/chemical.rb
@@ -4,7 +4,14 @@ class Chemical < ApplicationRecord
   has_many :carts, through: :cart_chemicals
 
   validates :product_name, :compound_product, :type_product, :area, :measurement_unit, :amount, presence: true
+  validates :product_name, uniqueness:
+  TYPE_PRODUCTS = ["INSETICIDA", "Adjuvante e Redutor de Espuma", "Fungicida", "FEROMÔNIO SINTÉTICO",
+                   "Adjuvante e Redutor de ph", "Fertilizante Foliar", "NEMATICIDA E INSETICIDA",
+                   "Acaricida", "Herbicida", "Adubo", "NEMATICIDA", "HERBICIDA", "REGULADOR DE CRESCIMENTO",
+                   "HERBICIDA E REGULADOR DE CRESCIMENTO", "FUNGICIDA", "Ferilizante Mineral Misto",
+                   "INSETICIDA E ACARICIDA", "Adjuvante", "Biofertilizante"]
 
+  AMOUNTS = [1, 5, 10, 20, 50, 100]
   # Search
   include PgSearch::Model
 

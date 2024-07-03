f038dfb0c560ad675d1803817dc4633439eb9349
Bernardo Carvalho <nal-carvalho@hotmail.com>
2024-07-03 13:23:53 -0300

    feat: new and create chemical

--- app/controllers/chemicals_controller.rb
+++ app/controllers/chemicals_controller.rb
@@ -4,4 +4,30 @@ class ChemicalsController < ApplicationController
     @chemical = Chemical.find(params[:id])
     authorize @chemical
   end
+
+  def new
+    @chemical = Chemical.new
+    authorize @chemical
+  end
+
+  # Creates a new chemical
+  def create
+    @chemical = Chemical.new(chemical_params)
+    authorize @chemical
+    if @chemical.save
+      flash[:alert] = "Químico criado com sucesso."
+
+      redirect_to root_path
+      # redirect_to mychemicals_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # Permits chemical parameters
+  def chemical_params
+    params.require(:chemical).permit(:product_name, :compound_product, :type_product, :area, :measurement_unit, :amount)
+  end
 end

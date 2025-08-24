2a7de34c35f58118b43e6bde7bea1371c2e6176a
Bernardo Carvalho <nal-carvalho@hotmail.com>
2025-08-24 16:08:32 -0300

    feat: inclusion of timeout for the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :timeoutable
 
   # Associations
   has_many :employees # , dependent: :destroy
@@ -15,8 +17,6 @@ class User < ApplicationRecord
   validates :cpf, :first_name, :last_name, presence: true
   validate :valid_cpf
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 
   def valid_cpf
     return if CPF.valid?(cpf)
